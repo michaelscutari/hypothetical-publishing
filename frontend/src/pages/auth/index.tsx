ce5f360b44c53f6f51c63db35905041510b65a93
pb217 <paula.barrow@duke.edu>
2026-01-10 00:41:16 -0500

    added MUI login page and routing

---
+++ frontend/src/pages/auth/index.tsx
@@ -0,0 +1,10 @@
+import {Routes, Route} from 'react-router-dom';
+import Login from './login';
+
+export default function Auth() {
+    return (
+        <Routes>
+            <Route path="/" element={<Login />} />
+        </Routes>
+    );
+}
