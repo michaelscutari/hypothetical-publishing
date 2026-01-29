db2877f7b6e463b256af4310afb5c6b35a89e344
pb217 <paula.barrow@duke.edu>
2026-01-29 17:23:44 -0500

    lint

--- frontend/src/components/MonthYearDatePicker.tsx
+++ frontend/src/components/MonthYearDatePicker.tsx
@@ -4,6 +4,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import type { Dayjs } from 'dayjs';
 import { TextField } from '@mui/material';
 
+//TODO: implement into pages that need month/year picker
 export default function MonthYearPicker({
   value,
   onChange,
@@ -18,6 +19,7 @@ export default function MonthYearPicker({
         format="MM/YYYY"
         value={value}
         onChange={(newValue) => onChange(newValue)}
+        slots={{ textField: TextField }}
         slotProps={
           {textField: { helperText: 'MM/YYYY', fullWidth: true } }
         }
