2d90c036baab7a5352b4a4821da25aa5397b4628
Daniel Rodriguez-Florido <dr285@duke.edu>
2026-02-01 20:22:48 -0500

    Feature SALES-3: Implements cascade delete for sales pertaining to a deleted book

--- backend/src/main/java/edu/duke/bookpublishing/sales/Sale.java
+++ backend/src/main/java/edu/duke/bookpublishing/sales/Sale.java
@@ -13,6 +13,8 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -39,6 +41,7 @@ public class Sale {
 
   @ManyToOne(optional = false)
   @JoinColumn(name = "book_id", nullable = false)
+  @OnDelete(action = OnDeleteAction.CASCADE)
   private Book book;
 
   @Min(1)
