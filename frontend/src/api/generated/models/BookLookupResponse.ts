54299095a50446a5130b61a9fe192a5e153c73de
Daniel Rodriguez-Florido <dr285@duke.edu>
2026-02-23 11:06:02 -0500

    Client

--- frontend/src/api/generated/models/BookLookupResponse.ts
+++ frontend/src/api/generated/models/BookLookupResponse.ts
@@ -30,5 +30,9 @@ export type BookLookupResponse = {
      * Publication month (1-12)
      */
     publicationMonth?: number;
+    /**
+     * Cover image URL
+     */
+    coverImage?: string;
 };
 
