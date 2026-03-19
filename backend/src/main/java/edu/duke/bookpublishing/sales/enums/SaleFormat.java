1a3e2c649e6d7a8508518a4e31f815ad1c6db106
Daniel Rodriguez-Florido <dr285@duke.edu>
2026-03-19 03:19:03 -0400

    Initial Schema Changes

---
+++ backend/src/main/java/edu/duke/bookpublishing/sales/enums/SaleFormat.java
@@ -0,0 +1,11 @@
+package edu.duke.bookpublishing.sales.enums;
+
+/**
+ * Denotes the format of the book that was sold in a sale e.g. PRINT
+ *
+ * @author Daniel Rodriguez-Florido
+ */
+public enum SaleFormat {
+  PRINT,
+  EBOOK
+}
