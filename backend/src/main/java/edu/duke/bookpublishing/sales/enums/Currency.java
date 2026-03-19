40bf9e167919a266b4ec4fcd6e89ee61954fbaa7
Daniel Rodriguez-Florido <dr285@duke.edu>
2026-03-19 03:07:31 -0400

    just check

--- backend/src/main/java/edu/duke/bookpublishing/sales/enums/Currency.java
+++ backend/src/main/java/edu/duke/bookpublishing/sales/enums/Currency.java
@@ -1,28 +1,27 @@
 package edu.duke.bookpublishing.sales.enums;
 
 /**
- * This enum is to denote the different currencies in the world.
- * Follows the convention of the variance request posted on Ed by team 4.
- * Includes all countries that have an amazon domain.
- * 
+ * This enum is to denote the different currencies in the world. Follows the convention of the
+ * variance request posted on Ed by team 4. Includes all countries that have an amazon domain.
+ *
  * @author Daniel Rodriguez-Florido
  */
 public enum Currency {
-    AUD, // Australia
-    BRL, // Brazil
-    CAD, // Canada
-    CNY, // China
-    EGP, // Egypt
-    EUR, // Belgium, France, Germany, Italy, Netherlands, Spain
-    INR, // India
-    JPY, // Japan
-    MXN, // Mexico
-    PLN, // Poland
-    SAR, // Saudi Arabia
-    SGD, // Singapore
-    SEK, // Sweden
-    TRY, // Turkey
-    AED, // United Arab Emirates
-    GBP, // United Kingdom
-    USD, // United States
+  AUD, // Australia
+  BRL, // Brazil
+  CAD, // Canada
+  CNY, // China
+  EGP, // Egypt
+  EUR, // Belgium, France, Germany, Italy, Netherlands, Spain
+  INR, // India
+  JPY, // Japan
+  MXN, // Mexico
+  PLN, // Poland
+  SAR, // Saudi Arabia
+  SGD, // Singapore
+  SEK, // Sweden
+  TRY, // Turkey
+  AED, // United Arab Emirates
+  GBP, // United Kingdom
+  USD, // United States
 }
